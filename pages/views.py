3d336989756ec498a8808735d572477974267579
Richard <registrations@rcb1.me>
2014-07-17 03:47:06 -0500

    Trying to retrieve graph.

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django_facebook.api import get_facebook_graph
 
 def home( request ):
     try:
-        graph = user.get_offline_graph( )
+        #graph = user.get_offline_graph( )
+        graph = get_facebook_graph( request )
         fm = graph.get( 'me' )
     except:
         fm = ''
